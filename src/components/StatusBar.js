7c6e6ec401b23a9b26ccf934c23c7076884705ee
numtel <ben@latenightsketches.com>
2025-01-01 00:41:37 -0800

    Statusbar icons

--- src/components/StatusBar.js
+++ src/components/StatusBar.js
@@ -3,6 +3,10 @@ import {
   useAccount,
   useReadContracts,
 } from 'wagmi';
+import {
+  CheckCircleIcon,
+  ExclamationTriangleIcon,
+} from '@heroicons/react/16/solid';
 
 export default function StatusBar({ sesh, refreshCounter }) {
   const account = useAccount();
@@ -27,8 +31,14 @@ export default function StatusBar({ sesh, refreshCounter }) {
       {!account.isConnected ? 'Wallet Not Connected' :
         !sesh ? 'Not Logged In' :
         isLoading ? 'Loading key registration status...' :
-        data && pubKeyTx && data[0].result !== pubKeyTx.args[0] ? 'Registered key does not match. Cannot receive on this private account at this Ethereum address.' :
-        data && pubKeyTx && data[0].result === pubKeyTx.args[0] ? 'Registered key matches. Able to receive on this private account at this Ethereum address.' :
+        data && pubKeyTx && data[0].result !== pubKeyTx.args[0] ? <>
+          <ExclamationTriangleIcon className="h-5 w-5 mr-1 text-red-700 inline-block" />
+          Registered key does not match. Cannot receive on this private account at this Ethereum address.
+        </> :
+        data && pubKeyTx && data[0].result === pubKeyTx.args[0] ? <>
+          <CheckCircleIcon className="h-5 w-5 mr-1 text-green-700 inline-block" />
+          Registered key matches. Able to receive on this private account at this Ethereum address.
+        </>:
         'Unknown receive status.'}
     </div>
   </div>);
