10cc6b6285723d2898c33e22728874e579dd19b2
numtel <ben@latenightsketches.com>
2025-02-08 01:06:37 -0800

    Deploy using github actions instead of s3

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
+  base: '/wrap-on-privacy/',
   plugins: [
     {
       name: 'treat-js-files-as-jsx',
